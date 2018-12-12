5bd7b66e1e45d60b9b2b88a99780682abcb4ae5b
Nils-Anders Nøttseter <nilsan@machina.no>
2018-12-12 19:06:26 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,12 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Nils-Anders Nøttseter", "location": "Mars"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
 app.get('/api/work', (request, response) => {
-	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]');
+	response.send('[{"yearFrom": "0", "yearTo": "2000", "place": "Here", "comment": "Was doing stuff"}, {"yearFrom": "2000", "yearTo": "2018", "place": "There", "comment": "Making coffee"}]');
 	logger.info('Responded to GET request on /api/work')
 });
 
